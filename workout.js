217d8c0e2a22d7888206f39bff12ce0ec9fa0109
RasheedElKassed <relkassed@gmail.com>
2018-08-09 08:22:50 -0700

    Added errors and port listening

--- workout.js
+++ workout.js
@@ -22,3 +22,19 @@ app.get('/reset-table',function(req,res,next){
     })
   });
 });
+
+app.use(function (req, res) {
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://flip3.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
