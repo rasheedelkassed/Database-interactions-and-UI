4d08e8aba196c72d74aa21734bcfe876d3023dad
RasheedElKassed <relkassed@gmail.com>
2018-08-09 09:35:30 -0700

    Change SQL database location

--- dbcon.js
+++ dbcon.js
@@ -1,9 +1,7 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-  host  : 'localhost',
-  user  : 'elkasser',
+  host  : 'classmysql.engr.oregonstate.edu',
+  user  : 'cs290_elkasser',
   password: '8472',
-  database: 'student'
+  database: 'workout'
 });
-
-module.exports.pool = pool;
