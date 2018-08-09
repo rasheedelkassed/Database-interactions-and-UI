adbc73b2ba0f0a753410b3cf9509b634923026be
RasheedElKassed <relkassed@gmail.com>
2018-08-09 10:03:19 -0700

    phpMyAdmin added

--- dbcon.js
+++ dbcon.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-  host  : 'localhost',
-  user  : 'student',
-  password: 'default',
-  database: 'student'
+  host  : 'classmysql.engr.oregonstate.edu',
+  user  : 'cs290_elkasser',
+  password: '8472',
+  database: 'cs290_elkasser'
 });
 
 module.exports.pool = pool;
