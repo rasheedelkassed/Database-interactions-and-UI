85522904c20f96a2d6fbd382b0dcf8aa360445cf
RasheedElKassed <relkassed@gmail.com>
2018-08-09 22:19:39 -0700

    debugging statement

--- workout.js
+++ workout.js
@@ -64,7 +64,7 @@ app.get("/reset-table",function(req, res, next){
 });
 
 app.post("/delete-row", function(req, res, next) {
-	console.log(req);
+	console.log(req.body);
 	mysql.pool.query("DELETE FROM `workout` WHERE `id`= ?", [req.query.id], function(err, result){
 		if(err){
 			next(err);
