604b4a2cefc3b836fa6b36baebfe3c5822fcd626
RasheedElKassed <relkassed@gmail.com>
2018-08-09 17:26:29 -0700

    Added click event listeners to submit buttons

--- public/script.js
+++ public/script.js
@@ -1,11 +1,37 @@
 
+// Call functions on page load
+if(document.readyState === "loading"){
+	document.addEventListener("DOMContentLoaded", addSubButtonListeners);
+}else{
+}
 
-function actDeleteButton(id){
-	var buttonPressed = document.getElementById(id);
-	function(event){
-		var req = new XMLHttpRequest();
-		console.log("It works");
-		event.preventDefault();
+// Call the functions to add event listeners to all buttons on the page
+function addSubButtonListeners(){
+	// Add click events to delete buttons
+	var deleteSubButtons = document.getElementsByClassName("delete");
+	for(var i = 0; i < deleteSubButtons.length; i++){
+		deleteSubButtons[i].addEventListener("click", actDeleteButton);
 	}
+	
+	// Add click events to edit buttons
+	var editSubButtons = document.getElementsByClassName("edit");
+	for (var i = 0; i < editSubButtons.length; i++) {
+		editButtons[i].addEventListener("click", actEditButton);
+  }
+}
+
+// Delete the row of the table in MySQL and reload the table
+function actDeleteButton(event){
+	var req = new XMLHttpRequest();
+	console.log("It works");
+	event.preventDefault();
+	
+}
+
+// Edit the row of the table in MySQL and reload the table
+function actDeleteButton(event){
+	var req = new XMLHttpRequest();
+	console.log("It works TWICE");
+	event.preventDefault();
 	
 }
