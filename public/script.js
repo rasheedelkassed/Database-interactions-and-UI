55bf134c42b66c493bf170067fc59cf0d3b25ae4
RasheedElKassed <relkassed@gmail.com>
2018-08-10 19:33:30 -0700

    Debugging statements

--- public/script.js
+++ public/script.js
@@ -8,6 +8,7 @@ if(document.readyState === "loading"){
 
 // Draw the table
 function drawTable(rows){
+	console.log(rows);
 	var header = ["Name", "Reps", "Weight", "Date", "Unit"];
 	var table = document.createElement("table");
 	// create the header
@@ -24,7 +25,7 @@ function drawTable(rows){
 		var rowData = document.createElement("tr")
 		for(r in rows[i+1]){
 			var cellData = document.createElement("td");
-			cellData.textContent = rows[i+1].r;
+			cellData.textContent = rows[i+1][""+r];
 			rowData.appendChild(cellData);
 		}
 		table.appendChild(rowData);
